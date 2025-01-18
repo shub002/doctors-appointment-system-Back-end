8549e905fb44f83a87f915066d9e230a8d1fa561
Shubham Somawanshi <shubhamsomawanshi@Shubhams-MacBook-Pro.local>
2025-01-18 20:50:13 +0530

    updated code

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use('/api/appointments', appointmentRoutes);
 app.use('/api/users', userRoutes);
 
 // Port configuration
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
